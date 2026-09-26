ec2d80e62763cd48155ecc9bf9f79b276d729896
akimsupernova <akimmrci@gmail.com>
2026-09-26 07:26:52 +0700

    Delete hypr directory

--- hypr/custom/keybinds.lua
+++
@@ -1,7 +0,0 @@
-hl.bind("CTRL+SUPER+ALT+Slash", hl.dsp.exec_cmd("xdg-open ~/.config/hypr/custom/keybinds.lua"), {description = "Edit user keybinds"} )
-
-hl.bind("SUPER + mouse_up",   hl.dsp.exec_cmd("wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"))
-hl.bind("SUPER + mouse_down", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"))
-
-hl.bind("SUPER + ALT + K", hl.dsp.exec_cmd("qs -c tools ipc call tools calculator"), { description = "Quickshell: Calculator" })
---hl.bind("SUPER + ALT + O", hl.dsp.exec_cmd("qs -c tools ipc call tools monitor"), { description = "Quickshell: Monitor settings" })
--- hypr/monitors.lua
+++
@@ -1,5 +0,0 @@
-hl.monitor({
-    output = "HDMI-A-1",
-    scale = 1,
-    bitdepth = 8,
-})
